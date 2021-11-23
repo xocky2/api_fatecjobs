063d9ad6fcf1697f398c393bc6a4ce5b0adff74f
Giovanni Barros <giovanniphilotech@gmail.com>
2021-11-23 00:20:12 -0300

    Rotas : empresa,aluno, vaga e candidatura

---
+++ routes/empresa.js
@@ -0,0 +1,144 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+
+//retorna todas empresas
+router.get('/', (req, res, next) => {
+    if(req.body.nome_fantasia){
+        mysql.getConnection((error,conn)=>{
+            if(error){return res.status(500).send({error: error})}
+            conn.query(
+                `SELECT * FROM EMPRESA where nome_fantasia like ("%${req.body.nome_fantasia}%");`,
+                (error, resultado,fields) =>{
+                    if(error){return res.status(500).send({error: error})}
+                    return res.status(200).send({response: resultado});
+                   
+
+                }
+            )
+        });
+        //return res.status(200).send({response: req.body})
+    }else{
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error: error})}
+        conn.query(
+            'SELECT * FROM EMPRESA;',
+            (error, resultado,fields) =>{
+                if(error){return res.status(500).send({error: error})}
+                return res.status(200).send({response: resultado})
+            }
+        )
+    });
+    }
+});
+
+//login empresa
+router.post('/login',(req, res, next)=>{
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error: error})}
+        conn.query(
+            'SELECT * FROM empresa WHERE email = ? AND senha= ?;',
+            [req.body.email,req.body.senha],
+            (error, resultado,fields) =>{
+                conn.release();
+                if(error){return res.status(500).send({error: error})}
+                if (resultado.length){
+                    return res.status(200).send({response: resultado})
+                }else{
+                    return res.status(200).send({response: "Email ou senha incorretos."})
+                }
+                
+                
+            }
+        )
+    })
+
+});
+
+//insere uma empresa
+router.post('/', (req, res, next) => {
+    if ( req.body.senha === req.body.confirmPassword){
+        mysql.getConnection((error,conn)=>{
+            if(error){return res.status(500).send({error: error})}
+    
+            conn.query(
+                'insert into empresa (email,senha,nome_fantasia,area_atuacao,cidade,bio,foto) values(?,?,?,?,?,?,?);',
+                [req.body.email, req.body.senha, req.body.nome_fantasia, req.body.area_atuacao, req.body.cidade, req.body.bio, req.body.foto],
+                
+                (error, resultado, field) => {
+                    conn.release();
+                    if(error){
+                        if (error.errno == 1062){
+                            return res.status(500).send({response: "Email já cadastrado"})
+                        }
+                        return res.status(500).send({error: error})
+                    }
+    
+                    res.status(201).send({
+                        mensagem: 'Empresa cadastrada com sucesso',
+                        idempresa: resultado.insertId
+                     });
+                }
+                
+            )
+        });
+    }else{
+        return res.status(500).send({response: "Senha não coincidem"})
+    }
+    
+
+});
+
+
+//altera empresa
+router.patch('/', (req, res, next) => {
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error: error})}
+        conn.query(
+            'UPDATE empresa SET email =?, senha = ?, nome_fantasia = ?, area_atuacao = ?,cidade = ?, bio = ?, foto = ? where idempresa = ?;',
+            [req.body.email, req.body.senha, req.body.nome_fantasia, req.body.area_atuacao, req.body.cidade, req.body.bio, req.body.foto, req.body.idempresa],
+            
+            (error, resultado, field) => {
+                conn.release();
+                if(error){return res.status(500).send({error: error})}
+
+                if (resultado.affectedRows == 0 ) {
+                    return res.status(500).send({response: "Empresa não alterada"})
+                }else{
+                    res.status(202).send({response: 'Dados alterados com sucesso'});
+                }
+                
+            }
+            
+        )
+    });
+});
+
+//deleta empresa
+router.delete('/', (req, res, next) => {
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error: error})}
+        conn.query(
+            'DELETE FROM empresa WHERE idempresa= ?',
+            [req.body.idempresa],
+            
+            (error, resultado, field) => {
+                conn.release();
+                if(error){
+                    return res.status(500).send({error: error})
+                }
+
+                if (resultado.affectedRows == 0 ) {
+                    return res.status(500).send({response: "empresa não excluida"})
+                }else{
+                    res.status(202).send({mensagem: 'Empresa excluida com sucesso'});
+                }
+                
+            }
+            
+        )
+    });
+});
+
+
+module.exports = router;
--- routes/vaga.js
+++ routes/vaga.js
@@ -1,67 +1,139 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
-
-//retorna todas as vagas
+//retorna todas vagas ou se passar por parâmetro body o id da empresa
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Retorna as vagas'
+    if(req.body.idempresa){
+        mysql.getConnection((error,conn)=>{
+            if(error){return res.status(500).send({error: error})}
+            conn.query(
+                `SELECT * FROM vaga where idempresa_fk = ?;`,
+                [req.body.idempresa],
+                (error, resultado,fields) =>{
+                    if(error){return res.status(500).send({error: error})}
+                    return res.status(200).send({response: resultado});
+
+                }
+            )
+        });
+    }else{
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error: error})}
+        conn.query(
+            'SELECT * FROM vaga;',
+            (error, resultado,fields) =>{
+                if(error){return res.status(500).send({error: error})}
+                return res.status(200).send({response: resultado})
+            }
+        )
     });
+    }
+});
+
+// candidaturas passando por parâmetro body o idaluno ou
+router.get('/candidatura', (req, res, next) => {
+    if(req.body.idaluno){
+        mysql.getConnection((error,conn)=>{
+            if(error){return res.status(500).send({error: error})}
+            conn.query(
+                `SELECT * FROM vaga where idaluno_fk = ?;`,
+                [req.body.idaluno],
+                (error, resultado,fields) =>{
+                    if(error){return res.status(500).send({error: error})}
+                    return res.status(200).send({response: resultado});
+
+                }
+            )
+        });
+    }else if (req.body.idvaga){
+        mysql.getConnection((error,conn)=>{
+            if(error){return res.status(500).send({error: error})}
+            conn.query(
+                `SELECT * FROM vaga where idvaga_fk = ?;`,
+                [req.body.idvaga],
+                (error, resultado,fields) =>{
+                    if(error){return res.status(500).send({error: error})}
+                    return res.status(200).send({response: resultado});
+
+                }
+            )
+        });
+    }
 });
 
 //insere uma vaga
 router.post('/', (req, res, next) => {
-    const vaga = {
-        descricao: req.body.descricao,
-        localizacao: req.body.localzacao,
-        salario: req.body.salario,
-        tipo: req.body.tipo
-    }
-
-    res.status(201).send({
-        mensagem: 'Vaga cadastrada',
-        vagaCadastrada: vaga
-     });
-});
-
-//retorna  vagas de acordo com o ID do ALUNO
-router.get('/:id_aluno',(req, res, next)=>{
-    const id = req.params.id_aluno;
-    if(id == 1){
-        res.status(200).send({
-            mensagem: 'Vagas XYZ do ALUNO',
-            id: id
-        })
-    }else {
-        res.status(200).send({
-            mensagem: 'Usange o GET de uma vaga especifica id genérico'
+        mysql.getConnection((error,conn)=>{
+            if(error){return res.status(500).send({error: error})}
+    
+            conn.query(
+                'insert into vaga (idempresa_fk,descricao,localizacao,salario,tipo) values(?,?,?,?,?);',
+                [req.body.idempresa, req.body.descricao, req.body.localizacao, req.body.salario, req.body.tipo],
+                (error, resultado, field) => {
+                    conn.release();
+                    if(error){return res.status(500).send({error: error})}
+                    res.status(201).send({
+                        mensagem: 'Vaga cadastrada com sucesso',
+                        idempresa: resultado.insertId
+                     });
+                }
+                
+            )
         });
-    };
-
-});
-
-//retorna uma vaga de acordo com o ID da EMPRESA
-router.get('/:id_empresa',(req, res, next)=>{
-    const id = req.params.id_empresa;
-    if(id == 1){
-        res.status(200).send({
-            mensagem: 'Vagas XYZ da empresa',
-            id: id
-        })
-    }else {
-        res.status(200).send({
-            mensagem: 'Usange o GET de uma vaga especifica id genérico'
-        });
-    };
+    
 
 });
 
 
-//deleta uma vaga
+//altera vaga
+router.patch('/', (req, res, next) => {
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error: error})}
+        conn.query(
+            'UPDATE vaga SET descricao = ?,localizacao = ?,salario = ?,tipo = ?',
+            [req.body.descricao, req.body.localizacao, req.body.salario, req.body.tipo],
+            
+            (error, resultado, field) => {
+                conn.release();
+                if(error){return res.status(500).send({error: error})}
+                if (resultado.affectedRows == 0 ) {
+                    return res.status(500).send({response: "Vaga não alterada"})
+                }else{
+                    res.status(202).send({response: 'Vaga alterada com sucesso'});
+                }
+                
+            }
+            
+        )
+    });
+});
+
+//deleta vaga
 router.delete('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Vaga excluida com sucesso'
-     });
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error: error})}
+        conn.query(
+            'DELETE FROM vaga WHERE idvaga= ?',
+            [req.body.idempresa],
+            
+            (error, resultado, field) => {
+                conn.release();
+                if(error){
+                    return res.status(500).send({error: error})
+                }
+
+                if (resultado.affectedRows == 0 ) {
+                    return res.status(500).send({response: "Vaga não excluida"})
+                }else{
+                    res.status(202).send({mensagem: 'Vaga excluida com sucesso'});
+                }
+                
+            }
+            
+        )
+    });
 });
+
 
 module.exports = router;
