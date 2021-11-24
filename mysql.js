ebe23530c42703687023ae6a2e3956076dc40956
Giovanni Barros <giovanniphilotech@gmail.com>
2021-11-24 02:50:39 -0300

    banco

--- mysql.js
+++ mysql.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
     "user": process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
     "database": process.env.MYSQL_DATABASE,
-    "host": process.env.MYSQL_HOST ,
+    "host": process.env.MYSQL_HOST,
     "port" : process.env.MYSQL_PORT
 });
 
