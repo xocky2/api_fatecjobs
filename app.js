ee02bcb46e8aa41b221b107bce98631558645fd0
Giovanni Barros <giovanniphilotech@gmail.com>
2021-12-02 18:41:03 -0300

    cors fix try 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 //const bodyParser = require('body-parser');
-const cors = require("cors");
+const cors = require('cors');
 
 
 const rotaAluno = require('./routes/aluno');
